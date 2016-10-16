0b3f875e8c47736839461188a5c125660717a80a
Jeff Gulbronson <jgulbronson@gmail.com>
2016-10-16 19:08:25 -0400

    Need to wire up job search

---
+++ static/jsx/job_search.jsx
@@ -0,0 +1,37 @@
+var JobSearchPage = React.createClass({
+  render: function() {
+    return (
+      <div className="container">
+        <div className="row mb20">
+          <div className="col-md-offset-2 col-md-8">
+            <span className="search-title">
+              Search for your next internship
+             </span>
+          </div>
+        </div>
+        <div className="row mb20">
+          <div className="col-md-offset-2 col-md-8">
+            <div className="secondary-header">
+              Who would you like to work for?
+             </div>
+             <input className="mb20 full-width" placeholder="Company" />
+             <div className="secondary-header">
+               Where would you like to work?
+              </div>
+              <input className="full-width" placeholder="Location" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-offset-2 col-md-8">
+            <button type="button">Search</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+    <JobSearchPage />,
+    document.getElementById('react-placeholder')
+    );
