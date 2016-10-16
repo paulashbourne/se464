2b179024e65c58ac1a6519df3589d9d0bcc3cd70
Marcus Sjolin <marcus.sjolin@live.ca>
2016-10-16 18:06:48 -0400

    Some job endpoints

--- api.py
+++ api.py
@@ -1,9 +1,19 @@
 from core import Blueprint
 from flask import request
-from models import Employer
+from models.employer import Employer
+from models.job import Job
 import ujson
 
 api = Blueprint('api', __name__)
+
+@api.post('/employer')
+def create_employer():
+    if 'employer' not in request.args:
+        # TODO: throw error here
+        pass
+
+    employer = Employer.create(request.args.get('employer'))
+    employer.save()
 
 @api.get('/employer/<employer_id>')
 def get_employer(employer_id):
@@ -11,14 +21,35 @@ def get_employer(employer_id):
     if employer is None:
         # TODO: throw error here
         pass
+
     return ujson.dumps(employer)
 
-@api.post('/employer')
-def create_employer():
-    if 'employer' not in request.args:
-        # throw error
+@api.post('/job')
+def create_job():
+    if 'job' not in request.args:
+        # TODO: throw error
         pass
 
-    employer = Employer.create(request.args.get('employer'))
-    employer.save() 
+    job = Job.create(request.args.get('job'))
+    job.save()
+
+@api.get('/jobs')
+def get_jobs():
+    query = {}
+
+    if 'employer_id' in request.args:
+        query['employer_id'] = request.args.get('employer_id')
+
+    if 'company_name' in request.args:
+        query['company_name'] = request.args.get('company_name')
+
+    if 'location' in request.args:
+        query['location'] = request.args.get('location')
+
+    jobs = Job.find_by(query)
+    if jobs is None:
+        # TODO: throw error
+        pass
+
+    return ujson.dumps(jobs)
 
