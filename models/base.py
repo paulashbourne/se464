8a34e29c2c10a22a6f7688cb24873faa7fbbca9e
Paul Ashbourne <paul.ashbourne@gmail.com>
2016-10-16 17:33:31 -0400

    Update base model

--- models/base.py
+++ models/base.py
@@ -2,3 +2,7 @@ from mongoengine import Document
 
 class BaseDocument(Document):
     meta = {'allow_inheritance': True}
+
+    @classmethod
+    def create(cls, **kwargs):
+        return cls(**kwargs)
