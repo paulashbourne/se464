d65b82b6939bfd63d7b94708ac0da90b6b9a1207
Paul Ashbourne <paul.ashbourne@gmail.com>
2016-10-16 20:14:59 -0400

    Helper functions

--- models/base.py
+++ models/base.py
@@ -58,45 +58,15 @@ class BaseDocument(Document):
 
     @classmethod
     def by_id(cls, id):
-        return cls.db_session.query(cls).filter_by(id = id).first()
+        return cls.find_one({'_id' : id})
 
     @classmethod
     def by_ids(cls, ids):
-        return cls.db_session.query(cls).filter(cls.id.in_(ids)).all()
+        return cls.find({'_id' : {'$in' : ids}})
 
     @classmethod
     def by_ids_dict(cls, ids):
         return dict([(c.id, c) for c in cls.by_ids(ids)])
-
-    @classmethod
-    def create(cls, commit = True, **kwargs):
-        item = cls(**kwargs)
-        cls.db_session.add(item)
-        if commit:
-            cls.db_session.commit()
-            cls.db_session.refresh(item)
-        return item
-
-    @classmethod
-    def default_order(cls):
-        return cls.id.asc()
-
-    @classmethod
-    def apply_query_options(cls, query, **kwargs):
-        if 'search' in kwargs and kwargs['search']:
-            if not kwargs['search_field']:
-                raise RuntimeError("Search field not specified")
-            column = cls.get_column(kwargs['search_field'])
-            query = query.filter(column.like("%%%s%%" % kwargs['search']))
-        if 'order_by' in kwargs:
-            query = query.order_by(cls.get_sort(*kwargs['order_by']))
-        else:
-            query = query.order_by(cls.default_order())
-        if 'start' in kwargs:
-            query = query.offset(kwargs['start'])
-        if 'count' in kwargs:
-            query = query.limit(kwargs['count'])
-        return query
 
     @classmethod
     def validate_fields(cls, fields):
@@ -109,31 +79,8 @@ class BaseDocument(Document):
     def validate_update(self, updates):
         return self.validate_fields(updates)
 
-    @classmethod
-    def column_aliases(cls):
-        return {}
-
-    @classmethod
-    def get_column(cls, name):
-        aliases = cls.column_aliases()
-        if name in aliases:
-            name = aliases[name]
-        return BaseTable.get_key(cls, name)
-
-    @classmethod
-    def get_sort(cls, sort, direction):
-        column = cls.get_column(sort)
-        if direction == 'asc':
-            return column.asc()
-        else:
-            return column.desc()
-
-    @classmethod
-    def query(cls):
-        return cls.db_session.query(cls)
-
     def __repr__(self):
-        return "<%s(id='%d')>" % (
+        return "<%s(id='%s')>" % (
             self.__class__.__name__,
             self.id
         )
