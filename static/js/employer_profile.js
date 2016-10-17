963ddc995b8b72c2e59ac883e02f3c553f780028
jeremypf <jeremyjpf@gmail.com>
2016-10-16 21:59:24 -0400

    changed employer profile page to use bootstrap

--- static/js/employer_profile.js
+++ static/js/employer_profile.js
@@ -1,24 +1,3 @@
-var Profile = React.createClass({
-  displayName: "Profile",
-
-  render: function () {
-
-    return React.createElement(
-      "div",
-      null,
-      "St"
-    );
-  }
-});
-
-var Joblist = React.createClass({
-  displayName: "Joblist",
-
-  render: function () {
-
-    return React.createElement("div", null);
-  }
-});
 
 var EmployerProfile = React.createClass({
   displayName: "EmployerProfile",
@@ -27,10 +6,158 @@ var EmployerProfile = React.createClass({
     return React.createElement(
       "div",
       { className: "container" },
-      React.createElement(Profile, null),
-      React.createElement(JobList, null)
+      React.createElement(EmployerInfo, null),
+      React.createElement(EmployerListings, null)
     );
   }
 });
+
+class EmployerInfo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return React.createElement(
+      "div",
+      { className: "row" },
+      React.createElement(
+        "div",
+        { className: "row mb20" },
+        React.createElement(
+          "div",
+          { className: "col-md-offset-2 col-md-8" },
+          React.createElement(
+            "span",
+            { className: "search-title" },
+            "Your Profile"
+          )
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "row mb20" },
+        React.createElement(
+          "div",
+          { className: "col-md-offset-2 col-md-8" },
+          React.createElement(
+            "div",
+            { className: "col-md-2 secondary-header" },
+            "Name"
+          ),
+          React.createElement(
+            "div",
+            { className: "mb20 full-width" },
+            "this.props.employer.name"
+          ),
+          React.createElement(
+            "div",
+            { className: "col-md-2 secondary-header" },
+            "E-mail"
+          ),
+          React.createElement(
+            "div",
+            { className: "mb20 full-width" },
+            "this.props.employer.email"
+          ),
+          React.createElement(
+            "div",
+            { className: "col-md-2 secondary-header" },
+            "Website"
+          ),
+          React.createElement(
+            "div",
+            { className: "mb20 full-width" },
+            "this.props.employer.website"
+          ),
+          React.createElement(
+            "div",
+            { className: "col-md-2 secondary-header" },
+            "Description"
+          ),
+          React.createElement(
+            "div",
+            { className: "mb20 full-width" },
+            "this.props.employer.description"
+          )
+        )
+      )
+    );
+  }
+}
+
+class EmployerListings extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return React.createElement(
+      "div",
+      { className: "row" },
+      React.createElement(
+        "div",
+        { className: "row mb20" },
+        React.createElement(
+          "div",
+          { className: "col-md-offset-2 col-md-8" },
+          React.createElement(
+            "span",
+            { className: "search-title" },
+            "Your Job Listings"
+          )
+        )
+      ),
+      React.createElement(
+        "div",
+        { className: "row mb20" },
+        React.createElement(
+          "div",
+          { className: "col-md-offset-2 col-md-8" },
+          React.createElement(
+            "div",
+            { className: "col-md-2 secondary-header" },
+            "Name"
+          ),
+          React.createElement(
+            "div",
+            { className: "mb20 full-width" },
+            "this.props.employer.name"
+          ),
+          React.createElement(
+            "div",
+            { className: "col-md-2 secondary-header" },
+            "E-mail"
+          ),
+          React.createElement(
+            "div",
+            { className: "mb20 full-width" },
+            "this.props.employer.email"
+          ),
+          React.createElement(
+            "div",
+            { className: "col-md-2 secondary-header" },
+            "Website"
+          ),
+          React.createElement(
+            "div",
+            { className: "mb20 full-width" },
+            "this.props.employer.website"
+          ),
+          React.createElement(
+            "div",
+            { className: "col-md-2 secondary-header" },
+            "Description"
+          ),
+          React.createElement(
+            "div",
+            { className: "mb20 full-width" },
+            "this.props.employer.description"
+          )
+        )
+      )
+    );
+  }
+}
 
 ReactDOM.render(React.createElement(EmployerProfile, null), document.getElementById('react-placeholder'));
--- static/jsx/employer_profile.jsx
+++ static/jsx/employer_profile.jsx
@@ -1,36 +1,97 @@
-var Profile = React.createClass({
-  render: function() {
-
-    return (
-    	<div>St</div>
-    );
-  }
-});
-
-
-var Joblist = React.createClass({
-  render: function() {
-
-    return (
-    	<div>
-    		
-    	</div>
-    );
-  }
-});
-
 
 var EmployerProfile = React.createClass({
   render: function() {
     return (
       <div className="container">
-        <Profile />
-        <JobList />
+        <EmployerInfo />
+        <EmployerListings />
       </div>
     );
   }
 });
 
+
+class EmployerInfo extends React.Component {
+  constructor(props) {
+    super(props);    
+  }
+
+  render() {
+    return (
+
+      <div className="row">
+      	<div className="row mb20">
+          <div className="col-md-offset-2 col-md-8">
+            <span className="search-title">
+              Your Profile
+             </span>
+          </div>
+        </div>
+        <div className="row mb20">
+          <div className="col-md-offset-2 col-md-8">
+            <div className="col-md-2 secondary-header">
+              Name
+             </div>
+             <div className="mb20 full-width">this.props.employer.name</div>
+            <div className="col-md-2 secondary-header">
+              E-mail
+             </div>
+             <div className="mb20 full-width">this.props.employer.email</div>
+            <div className="col-md-2 secondary-header">
+              Website
+             </div>
+             <div className="mb20 full-width">this.props.employer.website</div>
+            <div className="col-md-2 secondary-header">
+              Description
+             </div>
+             <div className="mb20 full-width">this.props.employer.description</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class EmployerListings extends React.Component {
+  constructor(props) {
+    super(props);    
+  }
+
+  render() {
+    return (
+
+      <div className="row">
+      	<div className="row mb20">
+          <div className="col-md-offset-2 col-md-8">
+            <span className="search-title">
+              Your Job Listings
+             </span>
+          </div>
+        </div>
+        <div className="row mb20">
+          <div className="col-md-offset-2 col-md-8">
+            <div className="col-md-2 secondary-header">
+              Name
+             </div>
+             <div className="mb20 full-width">this.props.employer.name</div>
+            <div className="col-md-2 secondary-header">
+              E-mail
+             </div>
+             <div className="mb20 full-width">this.props.employer.email</div>
+            <div className="col-md-2 secondary-header">
+              Website
+             </div>
+             <div className="mb20 full-width">this.props.employer.website</div>
+            <div className="col-md-2 secondary-header">
+              Description
+             </div>
+             <div className="mb20 full-width">this.props.employer.description</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
     <EmployerProfile />,
