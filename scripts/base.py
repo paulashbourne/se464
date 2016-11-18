86b2ef687fb7d9067ff963f2b60b7072c030b44e
Jeff Gulbronson <jgulbronson@gmail.com>
2016-11-18 10:57:23 -0500

    Use proper environment for seed script

--- scripts/base.py
+++ scripts/base.py
@@ -1,3 +1,4 @@
+import os
 from app import App, Environment
 
 class Script(object):
@@ -5,7 +6,10 @@ class Script(object):
     description = "Some Script"
 
     def __init__(self, **kwargs):
-        self.app = App(Environment.DEVELOPMENT)
+        if 'MONGODB_URI' in os.environ:
+            self.app = App(Environment.PRODUCTION)
+        else:
+            self.app = App(Environment.DEVELOPMENT)
 
     def run(self):
         # Should be defined in the script
